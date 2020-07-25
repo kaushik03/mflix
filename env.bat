4827eb7ab3ee3ca8da99702bb26d301f5ac4675c
Kaushik Nagwekar <kaushik.nagwekar@gmail.com>
2020-07-25 20:53:56 +0530

    Mflix v1

---
+++ env.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=mflix/mflix.py
+set FLASK_DEBUG=false
+set MFLIX_DB_URI="mongodb+srv://analytics:analytics-password@mflix.pccrc.mongodb.net/mflix?retryWrites=true&w=majority" & REM Replace XXXX with your MongoDB Connection URI
---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FLASK_APP=mflix/mflix.py
+export FLASK_DEBUG=false
+export MFLIX_DB_URI="XXXX" # Replace XXXX with your MongoDB Connection URI
